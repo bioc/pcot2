669eae3be17dfc2c1b40ea1d5a3399863678e2f5
unknown <qson003@stat.auckland.ac.nz>
2007-07-31 02:06:27 +0000

    Fix type errors in the mannul files
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pcot2@26057 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/t2.permu.R
+++ R/t2.permu.R
@@ -1,29 +1,28 @@
 "t2.permu" <-
 function(x,imat,tstat,ind1,ind2,permu,iter,ncomp,dist.method){
   npath <- ncol(imat)
-  tmat <- matrix(0, nrow=iter, ncol=npath)
-  
-  for (k in 1:iter){
-    if (trunc(k/50) == k/50) cat("iter=", k, "\n")
-    if (permu=="ByColumn"){
+  tmat <- matrix(0, nrow = iter, ncol = npath)
+  tmat[1,] <- tstat
+  for (k in 2:iter) {
+    if (trunc(k/50) == k/50)
+      cat("iter=", k, "\n")
+    if (permu == "ByColumn") {
       nn <- ncol(x)
-      newD <- x[,sample(1:nn,nn)]}
-    if (permu=="ByRow"){
+      newD <- x[, sample(1:nn, nn)]
+    }
+    if (permu == "ByRow") {
       nn <- nrow(x)
-      newD <- x[sample(1:nn,nn),]}
-    
+      newD <- x[sample(1:nn, nn), ]
+    }
     dimnames(newD) <- dimnames(x)
-    
-    for (i in 1:npath){
-      dat <- newD[as.logical(imat[,i]),]
-      newdat <- pco(dat,ncomp,dist.method)
-      tmat[k,i] <- t2(newdat,ind1,ind2)[1]
+    for (i in 1:npath) {
+      dat <- newD[as.logical(imat[, i]), ]
+      newdat <- pco(dat, ncomp, dist.method)
+      tmat[k, i] <- t2(newdat, ind1, ind2)[1]
     }
   }
-  
-  p.per <- double(npath)
-  for (i in 1:npath)
-    p.per[i] <- (sum(abs(tmat[-1,i])>=abs(tstat[i]))+1)/iter
+  p.per <- c()
+  for (i in 1:npath) p.per[i] <- sum(abs(tmat[,i]) >= abs(tmat[1,i]))/iter
   return(p.per)
 }
 
